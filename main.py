9f808330df65c833c150a88a8c76ed36c70e1f22
QUDRAT FAZLI <S332634849@OCDSB>
2018-03-28 11:01:16 -0400

    Almost Done

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# <Qudrat Fazli>
 # March 28, 2018
 
-###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def CtoF (C):
+    """Enter a temperature in celsius to recieve the same temperature in farenheight."""
+    F = (1.8) * C + 32
+    return F
 
-###### uncomment this when you are ready to work on it
-#def FtoC ():
-#
+def FtoC (F):
+    """Enter a temperature in farenheight to recieve the same temperature in celsius."""
+    C = (0.55556) * (F-32)
+    return C
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+print('Enter 1 to convert a temperature to celsius and Enter 2 to convert a temperature to farenheight. Then enter the temperature to convert.')
+choice = int(input())
+temperature = int(input('now enter temperature:'))
+if choice == 1 and temperature >= -273.15:
+    print(CtoF(temperature))
+elif choice == 2 and temperature >= -459.67:
+    print(FtoC(temperature))
+else:
+    print('Please enter a temperature value > -273.15 if trying to convert celsius to farenheit and a temperature value > -459.67 to convert farenheight to celsius. Also enter either 1 to 2 for your choice.')
+    
+
